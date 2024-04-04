9fba4f039ae682922c868792b6b399ea549b7aba
Szymczek <szymon.szymczewski@protonmail.com>
2024-04-04 14:36:57 +0200

    Added recipes and layout

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { FooterComponent } from './footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, RecipesComponent, ShoppingListComponent],
+  imports: [RouterOutlet, HeaderComponent, RecipesComponent, ShoppingListComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/src/app/recipes/recipe-list/recipe-list.component.ts
+++ frontend/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RecipeItemComponent } from './recipe-item/recipe-item.component';
+import { Recipe } from '../recipe.model';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-recipe-list',
   standalone: true,
-  imports: [RecipeItemComponent],
+  imports: [CommonModule, RecipeItemComponent],
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('Test recipe', 'test desc', 'https://www.allrecipes.com/thmb/X7YTvuaWALmH382xmOpfBQ_68mo=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/AR_RR_InstantPotBolognese_STILLSDSC07203-4x3-d29912d240184b409416d691e314284b.jpg'),
+    new Recipe('Test recipe 2', 'test desc 2', 'https://www.allrecipes.com/thmb/X7YTvuaWALmH382xmOpfBQ_68mo=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/AR_RR_InstantPotBolognese_STILLSDSC07203-4x3-d29912d240184b409416d691e314284b.jpg')
+  ];
 
 }
