516d7ea23bfa1c087d58eb45a67cff13da57ebe0
Szymczek <szymon.szymczewski@protonmail.com>
2024-04-05 15:34:14 +0200

    navigation with event binding

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
@@ -7,9 +8,13 @@ import { FooterComponent } from './footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, RecipesComponent, ShoppingListComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, RecipesComponent, ShoppingListComponent, FooterComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  loadedFeature: string = 'recipe';
+  onNavigate(feature: string){
+    this.loadedFeature = feature;
+  }
 }
